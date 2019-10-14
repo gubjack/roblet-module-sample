41495f8dce298aadf45b1fec8f7d313db2f16e1f
Hagen Stanek <stanek@roblet.org>
2019-10-14 17:46:28 +0000

    Added minimum client program

---
+++ client/src/main/java/org/roblet/sample/client/Main.java
@@ -0,0 +1,28 @@
+package org.roblet.sample.client;
+
+import java.io.Serializable;
+
+import org.roblet.Roblet;
+import org.roblet.Robot;
+
+import genRob.genControl.client.Client;
+import genRob.genControl.client.Server;
+import genRob.genControl.client.Slot;
+
+public class Main {
+
+    public static void  main (String[] args) throws Exception {
+        Client  c = new Client();
+        Server  s = c.getServer("localhost");
+        Slot  x = s.getSlot();
+        Roblet r = new TheRoblet();
+        x.run(r);
+    }
+
+    public static class  TheRoblet implements Roblet, Serializable {
+        public Object execute(Robot robot) throws Exception {
+            return null;
+        }
+    }
+
+}
